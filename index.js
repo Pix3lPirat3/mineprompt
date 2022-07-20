818097540ee617c5902e62ef1fd473105678ede4
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-07-19 21:40:16 -0700

    Open URL in User Browser
    Sends clicked URLs to be opened in the user's browser, instead of in an Electron Window

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // main.js
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   // Create the browser window.
@@ -17,44 +17,50 @@ const createWindow = () => {
       contextIsolation: false,
       backgroundThrottling: false // Keep 'false' (Performance drops when window is minimized)
     }
-  })
+  });
 
-  mainWindow.removeMenu()
+  mainWindow.removeMenu();
+
+  // Send clicked links (such as wiki) to the user's browser
+  mainWindow.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('window/index.html')
+  mainWindow.loadFile('window/index.html');
 
   mainWindow.webContents.on('before-input-event', (event, input) => {
     if (input.control && input.shift && input.key.toLowerCase() === 'i') {
-      event.preventDefault()
-      mainWindow.webContents.openDevTools()
+      event.preventDefault();
+      mainWindow.webContents.openDevTools();
     }
-  })
+  });
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 
-})
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
