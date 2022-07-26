5e50406f267adf36ff23648a0395293a1434b8dd
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-07-25 18:30:59 -0700

    Added "follow" command

---
+++ addons/commands/follow.js
@@ -0,0 +1,28 @@
+module.exports = {
+  addon: {
+    cmd: 'follow',
+    //aliases: ['near'],
+    args: ['nearest', 'stop'],
+    autocomplete: ['players', 'args'],
+    usage: 'follow <player | nearest>',
+    description: 'Follow a target player.',
+    useLanguage: true,
+    handler: async function(sender, args) {
+      let lang = this.lang;
+      if (!bot?.entity) return echo(lang.no_bot);
+      if(!args[0]) return echo(lang.no_argument);
+
+      let target = args[0].toLowerCase();
+      let entity = null;
+      if(target === 'stop') return bot.pathfinder.stop(), echo(lang.stopped);
+      if(target === 'nearest') entity = bot.nearestEntity(e => e.type === 'player');
+      if(!entity) entity = Object.values(bot.players).filter(e => e.username.toLowerCase() === target)[0]?.entity;
+      if(!entity) return echo(lang.no_target_found.replace('{type}', target));
+
+      bot.pathfinder.setGoal(new GoalFollow(entity, 2), true);
+
+      echo(lang.started.replace('{target}', entity.username));
+      
+    }
+  }
+};
