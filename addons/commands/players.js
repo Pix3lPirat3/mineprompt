9243f6628e0545a3f81744df872f1e22825c3eb0
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-07-31 03:17:23 -0700

    Update players.js

--- addons/commands/players.js
+++ addons/commands/players.js
@@ -1,17 +1,13 @@
 module.exports = {
   addon: {
     cmd: 'players',
-    //args: ['nearest', 'stop'],
-    //autocomplete: ['players', 'args'],
     usage: 'players',
     description: 'Show online players.',
     useLanguage: true,
     handler: async function(sender, args) {
       let lang = this.lang;
       if (!bot?.entity) return echo(lang.no_bot);
-
       let map = Object.keys(bot.players).map(e => e.replace('{name}', e.username));
-
       echo(lang.player_msg.replace('{map}', map.join(', ')));
     }
   }
