ccc6505deedf9f7061dd352f0103fd74c46f2b5b
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-07-31 03:15:43 -0700

    Added Eat, Players Commands

---
+++ addons/commands/eat.js
@@ -0,0 +1,24 @@
+module.exports = {
+  addon: {
+    cmd: 'eat',
+    //args: [''],
+    //autocomplete: ['players', 'args'],
+    usage: 'eat <item>',
+    description: 'Follow a target player.',
+    useLanguage: true,
+    handler: async function(sender, args) {
+      let lang = this.lang;
+      if (!bot?.entity) return echo(lang.no_bot);
+
+      utils.eat(14, ['beetroot', 'cooked_beef', 'apple', 'pumpkin_pie']).then(function(item) {
+        echo(`I ate a(n) ${item.name}`)
+      }).catch(function(e) {
+        if(e.message === 'threshold') return echo(`The bot was too full to eat, skipping.`);
+        if(e.message === 'no_food') return echo(`We have no items to eat!`);
+        console.log(e)
+      }).done();
+
+      
+    }
+  }
+};
