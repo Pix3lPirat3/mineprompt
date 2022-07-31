e37e26bf1dab53491dcd4cf337b880cfbae43774
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-07-31 03:16:04 -0700

    Added Utils

--- internal/addonManager.js
+++ internal/addonManager.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var utils = require('./../utils.js');
 
 module.exports = {
 
--- utils.js
+++ utils.js
@@ -1,16 +1,25 @@
 module.exports = {
-  eat: async function() {
-    var foodList = bot.registry.foodsByName;
-    const foodItems = bot.inventory.items().filter((item) => item.name in foodList).sort((a, b) => foodList[b.name]['foodPoints'] - foodList[a.name]['foodPoints']);
-    if (bot.food < 6 && !foodItems.length) {
-      return echo(`[[;#AAAAAA;]Disconnected: "[[;#FF5555;]Safety Kick (Starvation)]"`);
-    }
-    if (bot.food <= 16 && foodItems.length) {
-      let food = foodItems[0];
-      console.log('I NEED FOOD.')
-      await bot.equip(food)
-      return bot.consume();
-    }
+  /*
+    threshold - Once bot.food is lower than this, we can eat the item.
+    filter - (Optional) An array of items to eat ['apple', 'cooked_beef']
+  */
+  eat: function(threshold, filter = []) {
+    return new Promise(async function(resolve, reject) {
+      if (bot.food > threshold) return reject(new Error("threshold")); // The bot had more hunger than the threshold
+      let items = getItems(); // Get a list of food items sorted by food points (Optional : filter)
+      if (!items.length) return reject(new Error("no_food")); // We have no edible items
+      await bot.equip(items[0].id, 'hand');
+      await bot.consume()
+      return resolve(items[0]);
+
+      function getItems() {
+        let foodByPoints = Object.values(bot.registry.foods).sort((a, b) => b.foodPoints - a.foodPoints); // Sort food items by foodPoints (Yes, I know about foodsByFoodPoints)
+        let filtered = filter.length ? foodByPoints.filter(i => filter.includes(i.name)) : foodByPoints; // If there's a filter use it, use foodByPoints list
+        let edible = filtered.filter(i => bot.inventory.items().map(e => e.name).includes(i.name))
+        return edible;
+      }
+
+    })
   }
 
 }
