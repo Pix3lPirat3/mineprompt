c8a4a41f7b09757aeb2b0bda6b79c8305f649bd2
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-07-31 06:10:27 -0700

    Added "cmd" command

---
+++ addons/commands/cmd.js
@@ -0,0 +1,17 @@
+module.exports = {
+  addon: {
+    cmd: 'cmd',
+    aliases: ['command'],
+    autocomplete: ['players'],
+    usage: 'cmd <command>',
+    description: 'Run a command.',
+    useLanguage: true,
+    handler: async function(sender, args) {
+      let lang = this.lang;
+      if (!bot?.entity) return echo(lang.no_bot);
+      var command = args.join(' ');
+      echo(lang.command_sent.replace('{cmd}', command))
+      bot.chat('/' + command);
+    }
+  }
+};
