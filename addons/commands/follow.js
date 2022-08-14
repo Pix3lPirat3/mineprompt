596095a204375c545b8b67edda0a6b99cc7db30b
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-08-13 18:11:37 -0700

    Patch (Removed nearestEntity fallback)

--- addons/commands/follow.js
+++ addons/commands/follow.js
@@ -14,11 +14,22 @@ module.exports = {
       let target = args[0].toLowerCase();
       let entity = null;
       if(target === 'stop') return bot.pathfinder.stop(), echo(lang.stopped);
-      if(target === 'nearest') entity = bot.nearestEntity(e => e.type === 'player');
-      if(!entity) entity = Object.values(bot.players).filter(e => e.username.toLowerCase() === target)[0]?.entity;
+      if(target === 'nearest') {
+        entity = bot.nearestEntity(e => e.type === 'player');
+        if(!entity) return echo(lang.no_target_found.replace('{type}', entity));
+        bot.pathfinder.setGoal(new GoalFollow(entity, 2), true);
+        return echo(lang.started.replace('{target}', entity.username));
+      }
+
+      if (target = Object.values(bot.players).filter(e => e.username === args[0])[0]?.entity) {
+       if (!target) return echo(lang.no_target_found.replace('{target}', args[0]));
+       bot.pathfinder.setGoal(new GoalFollow(target, 2), true);
+       return echo(lang.started.replace('{target}', args[0]));
+      }
+
+
       if(!entity) return echo(lang.no_target_found.replace('{type}', target));
 
-      bot.pathfinder.setGoal(new GoalFollow(entity, 2), true);
 
       echo(lang.started.replace('{target}', entity.username));
       
